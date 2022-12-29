0833ac7a5e8915ad0de2c3357d2fcd33668fa6dc
JunKi Yoon <na66421@gmail.com>
2022-12-29 23:39:37 +0900

    Add review comment

--- dto/user.dto.go
+++ dto/user.dto.go
@@ -4,6 +4,9 @@ package dto
 //	 Create Request
 /////////////////////////
 
+/*
+required를 통해서 필수 값에 대해서 validation을 check 하도록 구현하신 점 좋습니다.
+*/
 //메뉴 생성에 사용하는 데이터
 type CreateUserRequest struct {
 	Name string `json:"name" binding:"required" ` //사용자 이름 
--- router/menu/menu.routes.go
+++ router/menu/menu.routes.go
@@ -14,6 +14,24 @@ func InitWithSelf(menuController menu_controller.MenuController ) MenuRoute {
 	return MenuRoute{menuController: menuController}
 }
 
+/*
+엔드포인트 구성에 대해서 전반적인 코멘트 드립니다.
+1. REST API의 성숙도 모델에 대해서 공부해보시면 좋을 것 같습니다.
+
+2. 일반적으로 HTTP URI에 new, modify 와 같은 행위는 들어가지 않습니다. 
+	복수형의 단어로 구성을 하고, 동일한 URI 내에서 http method만 변경하여 행위를 표현하는 것이 일반적인 REST API의 구성 방식입니다.
+	e.g.
+	GET v1/menus -> 메뉴 목록을 조회.
+	GET v1/menus/1 -> 1번 메뉴를 조회.
+	POST v1/menus -> 메뉴를 생성.
+	PATCH v1/menus/1 -> 1번 메뉴에 대해서 업데이트
+	DELETE v1/menus/1 -> 1번 메뉴에 대해서 삭제
+*/
+
+/*
+각 라우팅을 주제에 맞게 다른 파일로 분리해주신 점 좋습니다.
+이렇게 구성하면 추후 확장성에 있어서도 용이해 보입니다.
+*/
 
 func (r *MenuRoute) InitWithRoute(server *gin.Engine) {
 	storeMenuRouterV1 := server.Group("/api/v1/store")
